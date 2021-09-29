e0354299825ae9742cf83a8e0d1f1a222e5116aa
salman alfariji <79994933+salmanti20@users.noreply.github.com>
2021-09-29 14:57:58 +0700

    Add files via upload

---
+++ latihan1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author salma
+ */
+public class latihan1 {
+    public static void main(String[] args){
+        int duit = 50000;
+        int harga =55000;
+        String hasil = duit > harga ? duit + "uangnya kurang de" : duit + " uang dede gak cukup kalo beli mainan ini "; 
+        System.out.println(hasil);
+    }
+}
---
+++ latihan2.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author salma
+ */
+class latihan2 {
+  public static void main(String args[]){
+         
+    int a = 100;
+    int b = 20;
+    int c = 528; 
+    int d = 70;
+    int e = 256; 
+    int f = 3;
+    int g = 128; 
+    int h = 5;
+    int i = 600; 
+    int j = 4;
+    int k = 423;  
+    int l = 2;
+    int hasil;
+      
+    hasil = a & b;
+    System.out.println("Hasil perhitungan dari a & b = " + hasil );  
+    System.out.println("--------------------------------");
+    
+    hasil = c | d;
+    System.out.println("Hasil perhitungan dari c | d = " + hasil );
+    System.out.println("--------------------------------");
+      
+    hasil = e >> f;
+    System.out.println("Hasil perhitungan dari a >> 1 = " + hasil );
+    System.out.println("--------------------------------");
+      
+    hasil = g << h;
+    System.out.println("Hasil perhitungan dari b << 2 = " + hasil );
+    System.out.println("--------------------------------");
+       
+    hasil = i >> j;
+    System.out.println("Hasil perhitungan dari a >> 1 = " + hasil );  
+    System.out.println("--------------------------------");
+      
+    hasil = k << l;
+    System.out.println("Hasil perhitungan dari b << 2 = " + hasil );
+    System.out.println("--------------------------------");
+  }
+}
